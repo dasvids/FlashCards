418dba46d48dfbde07ff445fde75e272ac777f93
grchs <nedorubartem@gmail.com>
2024-02-15 13:24:35 +0300

    test commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Wawa 2 !</Text>
+      <Text>Wawa</Text>
       <StatusBar style="auto" />
     </View>
   );
